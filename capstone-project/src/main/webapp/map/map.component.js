ff33485b2cdbf62cc9f4af6071ab4aae8d8a5929
NaamaHolzer <naamaholzer1@gmail.com>
2020-09-14 14:54:17 +0000

    Let the user add a marker to the map

--- capstone-project/src/main/webapp/map/map.component.js
+++ capstone-project/src/main/webapp/map/map.component.js
@@ -6,8 +6,7 @@
 angular.module('map').component('mapComponent', {
     templateUrl: 'map/map.template.html',
     controller: function($scope) {
-
-        //Define some hard-coded markers to be shown on the map
+        // Define some hard-coded markers to be shown on the map
         $scope.markers = [{
             city: 'India',
             lat: 23.200000,
@@ -18,6 +17,7 @@ angular.module('map').component('mapComponent', {
             long: 83.3697
         }];
 
+
         let googleMapOption = {
             zoom: 4,
             center: new google.maps.LatLng(25, 80)
@@ -25,20 +25,75 @@ angular.module('map').component('mapComponent', {
 
         $scope.gMap = new google.maps.Map(document.getElementById('map-container'), googleMapOption);
 
-        // Add a marker to the map
-        let createMarker = function(marker) {
+        // When the user clicks on the map, show a marker with a text box the user can edit.
+        $scope.gMap.addListener('click', (event) => {
+            createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+        });
 
+        // Editable marker that displays when a user clicks on the map. 
+        let editMarker;
+
+        // Add a marker to the map
+        let createMarkerForDisplay = function(marker) {
             let markersInfo = new google.maps.Marker({
                 map: $scope.gMap,
                 position: new google.maps.LatLng(marker.lat, marker.long),
                 title: marker.city
             });
+            let winInfo = new google.maps.InfoWindow();
 
+            google.maps.event.addListener(markersInfo, 'click', function() {
+                winInfo.setContent('<h1>' + markersInfo.title + '</h1>');
+                winInfo.open($scope.gMap, markersInfo);
+            });
         };
 
-        //Iterate over the list of markers and add all of them to the map
+        // Creates a marker that shows a textbox the user can edit. 
+        let createMarkerForEdit = function(lat, lng) {
+            // If we're already showing an editable marker, then remove it.
+            if (editMarker) {
+                editMarker.setMap(null);
+            }
+
+            editMarker = new google.maps.Marker({ position: { lat: lat, lng: lng }, map: $scope.gMap });
+
+            const infoWindow = new google.maps.InfoWindow({ content: buildInfoWindowInput(lat, lng) });
+
+            // When the user closes the editable info window, remove the marker.
+            google.maps.event.addListener(infoWindow, 'closeclick', () => {
+                editMarker.setMap(null);
+            });
+
+            infoWindow.open($scope.gMap, editMarker);
+        }
+
+        // Build and return HTML elements that show an editable textbox and a submit button.
+        let buildInfoWindowInput = function(lat, lng) {
+            const textBox = document.createElement('textarea');
+            const button = document.createElement('button');
+            button.appendChild(document.createTextNode('Submit'));
+
+            button.onclick = () => {
+                let newMarker = {
+                    city: textBox.value,
+                    lat: lat,
+                    long: lng
+                };
+                createMarkerForDisplay(newMarker);
+                editMarker.setMap(null);
+            };
+
+            const containerDiv = document.createElement('div');
+            containerDiv.appendChild(textBox);
+            containerDiv.appendChild(document.createElement('br'));
+            containerDiv.appendChild(button);
+
+            return containerDiv;
+        }
+
+        // Iterate over the list of markers and add all of them to the map
         for (let i = 0; i < $scope.markers.length; i++) {
-            createMarker($scope.markers[i]);
+            createMarkerForDisplay($scope.markers[i]);
         }
 
     }
