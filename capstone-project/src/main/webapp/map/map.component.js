dc3f78741d5edd84ba847321e9c64c800c151e55
NaamaHolzer <naamaholzer1@gmail.com>
2020-09-16 06:31:15 +0000

    Change according to Or's notes

--- capstone-project/src/main/webapp/map/map.component.js
+++ capstone-project/src/main/webapp/map/map.component.js
@@ -26,9 +26,9 @@ angular.module('map').component('mapComponent', {
         $scope.gMap = new google.maps.Map(document.getElementById('map-container'), googleMapOption);
 
         // Add a marker to the map
-        let createMarker = function(marker) {
+        const addMarker = function(marker) {
 
-            let markersInfo = new google.maps.Marker({
+            new google.maps.Marker({
                 map: $scope.gMap,
                 position: new google.maps.LatLng(marker.lat, marker.long),
                 title: marker.city
@@ -38,7 +38,7 @@ angular.module('map').component('mapComponent', {
 
         // Iterate over the list of markers and add all of them to the map
         for (let i = 0; i < $scope.markers.length; i++) {
-            createMarker($scope.markers[i]);
+            addMarker($scope.markers[i]);
         }
 
     }
