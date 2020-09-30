f71c66a59106330476db09a5833b625c8e24506a
noyflat <noyflat@gmail.com>
2020-09-30 07:43:22 +0000

    Center text
    Add app name and description
    
    Change map to fit to page

--- capstone-project/src/main/angular-webapp/src/app/app.module.ts
+++ capstone-project/src/main/angular-webapp/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { MatSliderModule } from '@angular/material/slider';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -17,7 +20,9 @@ import { MapComponent } from './map/map.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
