c47429c34cfdface984de3dbeaa4ad600f1b4721
NaamaHolzer <naamaholzer1@gmail.com>
2020-10-01 09:56:07 +0000

    Let the user update the data of an existing marker

--- capstone-project/src/main/angular-webapp/src/app/map/map.component.ts
+++ capstone-project/src/main/angular-webapp/src/app/map/map.component.ts
@@ -49,10 +49,13 @@ export class MapComponent implements OnInit {
     function buildInfoWindowInput(lat, lng) {
       const animal = document.createElement('textarea');
       animal.placeholder = "Animal";
+      animal.className = 'animal';
       const description = document.createElement('textarea');
       description.placeholder = "Description";
+      description.className = 'description';
       const reporter = document.createElement('textarea');
       reporter.placeholder = "Reporter's Name";
+      reporter.className = 'reporter';
       const button = document.createElement('button');
       button.appendChild(document.createTextNode('Submit'));
 
@@ -121,12 +124,17 @@ export class MapComponent implements OnInit {
       deleteButton.onclick = () => {
         deleteMarker(markerData, markerForDisplay);
       };
-
+      const updateButton = document.createElement('button');
+      updateButton.appendChild(document.createTextNode('Update'));
+      updateButton.onclick = () => {
+        updateMarker(markerData, markerForDisplay);
+      }
       const containerDiv = document.createElement('div');
       containerDiv.appendChild(animal);
       containerDiv.appendChild(description);
       containerDiv.appendChild(reporter);
       containerDiv.appendChild(deleteButton);
+      containerDiv.appendChild(updateButton);
 
       return containerDiv;
     }
@@ -143,6 +151,45 @@ export class MapComponent implements OnInit {
       });
       // Remove marker from the map.
       markerForDisplay.setMap(null);
+    }
+
+    // Updates the data of an existing marker.
+    function updateMarker(markerData, markerForDisplay) {
+
+      const editableInfoWindow = new google.maps.InfoWindow({ content: buildUpdateInfoWindow(markerData, markerForDisplay) });
+      editableInfoWindow.open(gMap, markerForDisplay);
+    }
+
+    function buildUpdateInfoWindow(markerData,markerForDisplay) {
+      const animal = document.createElement('textarea');
+      animal.value = markerData.animal;
+      const description = document.createElement('textarea');
+      description.value = markerData.description;
+      const reporter = document.createElement('textarea');
+      reporter.value = markerData.reporter;
+      const updateButton = document.createElement('button');
+      updateButton.appendChild(document.createTextNode('Update'));
+      updateButton.onclick = () => {
+        const updatedMarker = {
+          animal: animal.value,
+          description: description.value,
+          reporter: reporter.value,
+          lat: markerData.lat,
+          lng: markerData.lng
+        };
+        deleteMarker(markerData,markerForDisplay);
+        postMarker(updatedMarker);
+        addMarkerForDisplay(updatedMarker);
+      };
+
+      const containerDiv = document.createElement('div');
+      containerDiv.appendChild(animal);
+      containerDiv.appendChild(description);
+      containerDiv.appendChild(reporter);
+      containerDiv.appendChild(document.createElement('br'));
+      containerDiv.appendChild(updateButton);
+
+      return containerDiv;
     }
 
     // Fetches markers from the backend and adds them to the map.
--- capstone-project/src/main/webapp/main.js
+++ capstone-project/src/main/webapp/main.js
@@ -210,10 +210,13 @@ class MapComponent {
         function buildInfoWindowInput(lat, lng) {
             const animal = document.createElement('textarea');
             animal.placeholder = "Animal";
+            animal.className = 'animal';
             const description = document.createElement('textarea');
             description.placeholder = "Description";
+            description.className = 'description';
             const reporter = document.createElement('textarea');
             reporter.placeholder = "Reporter's Name";
+            reporter.className = 'reporter';
             const button = document.createElement('button');
             button.appendChild(document.createTextNode('Submit'));
             button.onclick = () => {
@@ -273,11 +276,17 @@ class MapComponent {
             deleteButton.onclick = () => {
                 deleteMarker(markerData, markerForDisplay);
             };
+            const updateButton = document.createElement('button');
+            updateButton.appendChild(document.createTextNode('Update'));
+            updateButton.onclick = () => {
+                updateMarker(markerData, markerForDisplay);
+            };
             const containerDiv = document.createElement('div');
             containerDiv.appendChild(animal);
             containerDiv.appendChild(description);
             containerDiv.appendChild(reporter);
             containerDiv.appendChild(deleteButton);
+            containerDiv.appendChild(updateButton);
             return containerDiv;
         }
         // Deletes an existing marker.
@@ -292,6 +301,42 @@ class MapComponent {
             });
             // Remove marker from the map.
             markerForDisplay.setMap(null);
+        }
+        function updateMarker(markerData, markerForDisplay) {
+            const animal = document.createElement('textarea');
+            animal.value = markerData.animal;
+            const description = document.createElement('textarea');
+            description.value = markerData.description;
+            const reporter = document.createElement('textarea');
+            reporter.value = markerData.reporter;
+            const updateButton = document.createElement('button');
+            updateButton.appendChild(document.createTextNode('Update'));
+            updateButton.onclick = () => {
+                const updatedMarker = {
+                    animal: animal.value,
+                    description: description.value,
+                    reporter: reporter.value,
+                    lat: markerData.lat,
+                    lng: markerData.lng
+                };
+                deleteMarker(markerData, markerForDisplay);
+                postMarker(updatedMarker);
+                addMarkerForDisplay(updatedMarker);
+            };
+            const cancelButton = document.createElement('button');
+            cancelButton.appendChild(document.createTextNode('Cancel'));
+            cancelButton.onclick = () => {
+                editableInfoWindow.close();
+            };
+            const containerDiv = document.createElement('div');
+            containerDiv.appendChild(animal);
+            containerDiv.appendChild(description);
+            containerDiv.appendChild(reporter);
+            containerDiv.appendChild(document.createElement('br'));
+            containerDiv.appendChild(updateButton);
+            containerDiv.appendChild(cancelButton);
+            const editableInfoWindow = new google.maps.InfoWindow({ content: containerDiv });
+            editableInfoWindow.open(gMap, markerForDisplay);
         }
         // Fetches markers from the backend and adds them to the map.
         mapComponent.httpClient.get('/markers')
