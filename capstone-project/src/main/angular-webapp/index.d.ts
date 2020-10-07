2c4c80df732d858345fa31d8bf5be78db1c82e4b
noyflat <noyflat@gmail.com>
2020-10-07 08:40:47 +0000

    Add dependencies

--- capstone-project/src/main/angular-webapp/index.d.ts
+++ capstone-project/src/main/angular-webapp/index.d.ts
@@ -1,1 +1,2 @@
 declare module 'googlemaps';
+declare module 'gapi.auth2';
--- capstone-project/src/main/angular-webapp/src/app/authentication/authentication.component.ts
+++ capstone-project/src/main/angular-webapp/src/app/authentication/authentication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { } from 'gapi.auth2';
 
 @Component({
   selector: 'app-authentication',
@@ -9,7 +10,44 @@ export class AuthenticationComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  private gapiSetup;
+  private authInstance;
+  private user;
+  private error;
+
+  async initGoogleAuth(): Promise<void> {
+    //  Create a new Promise where the resolve 
+    // function is the callback passed to gapi.load
+    const pload = new Promise((resolve) => {
+      gapi.load('auth2', resolve);
+    });
+
+    // When the first promise resolves, it means we have gapi
+    // loaded and that we can call gapi.init
+    return pload.then(async () => {
+      await gapi.auth2
+        .init({ client_id: '' })
+        .then(auth => {
+          this.gapiSetup = true;
+          this.authInstance = auth;
+        });
+    });
+  }
+
+  async authenticate(): Promise<gapi.auth2.GoogleUser> {
+    // Initialize gapi if not done yet
+    if (!this.gapiSetup) {
+      await this.initGoogleAuth();
+    }
+
+    // Resolve or reject signin Promise
+    return new Promise(async () => {
+      await this.authInstance.signIn().then(
+        user => this.user = user,
+        error => this.error = error);
+    });
   }
 
 }
