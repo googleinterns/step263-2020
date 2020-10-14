e73bbdc8a560b58965e45a2d4ec3fc3f69538d1d
noyflat <noyflat@gmail.com>
2020-10-14 13:28:40 +0000

    Add comments

--- capstone-project/src/main/angular-webapp/src/app/authentication/authentication.component.ts
+++ capstone-project/src/main/angular-webapp/src/app/authentication/authentication.component.ts
@@ -12,18 +12,21 @@ export class AuthenticationComponent implements OnInit {
   constructor(private authService: SocialAuthService, private userService: UserService) { }
 
   ngOnInit(): void {
+    // When user status changes update the user service
     this.authService.authState.subscribe((user) => {
       this.userService.currentUser = user;
     });
   }
 
+  // Sign in user
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
 
+  // Sign out user and reload page
   signOut(): void {
     this.authService.signOut();
-    window.location.reload()
+    window.location.reload();
   }
 
   // Return the current user
--- capstone-project/src/main/angular-webapp/src/app/user.service.ts
+++ capstone-project/src/main/angular-webapp/src/app/user.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
+  // Holds the current user, updated in authentication component
   currentUser;
 
   constructor() {  }
