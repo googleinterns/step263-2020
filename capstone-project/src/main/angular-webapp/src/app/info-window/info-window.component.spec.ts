30a232eb53f159c7601e3cc27797341a5a5dcc64
NaamaHolzer <naamaholzer1@gmail.com>
2020-10-29 11:07:15 +0000

    Add tests to verify events are emitted

--- capstone-project/src/main/angular-webapp/src/app/info-window/info-window.component.spec.ts
+++ capstone-project/src/main/angular-webapp/src/app/info-window/info-window.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
+import 'jasmine';
 
 import { InfoWindowComponent } from './info-window.component';
 
@@ -6,20 +9,62 @@ describe('InfoWindowComponent', () => {
   let component: InfoWindowComponent;
   let fixture: ComponentFixture<InfoWindowComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ InfoWindowComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [InfoWindowComponent],
+      providers: [HttpClient, HttpHandler]
+    });
     fixture = TestBed.createComponent(InfoWindowComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create an infoWindowComponent instance', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should emit on submit', () => {
+
+    spyOn(component.submitEvent, 'emit');
+    // Trigger the submit event
+    component.submit("animal", "description", "reporter");
+
+    fixture.detectChanges();
+
+    expect(component.submitEvent.emit).toHaveBeenCalled();
+  });
+
+  it('Should emit on delete', () => {
+
+    spyOn(component.deleteEvent, 'emit');
+    // Trigger the delete event
+    component.delete();
+
+    fixture.detectChanges();
+
+    expect(component.deleteEvent.emit).toHaveBeenCalled();
+  });
+
+  it('Should emit on update', () => {
+
+    spyOn(component.updateEvent, 'emit');
+    // trigger the update event
+    component.update();
+
+    fixture.detectChanges();
+
+    expect(component.updateEvent.emit).toHaveBeenCalled();
+  });
+
+  it('Should emit on cancel', () => {
+
+    spyOn(component.cancelEvent, 'emit');
+    // trigger the cancel event
+    component.cancel();
+
+    fixture.detectChanges();
+
+    expect(component.cancelEvent.emit).toHaveBeenCalled();
+  });
 });
+
+
