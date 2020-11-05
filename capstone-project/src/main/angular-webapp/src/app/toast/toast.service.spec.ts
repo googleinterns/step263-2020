e145933aadec577c64a23b446a1cb13d0d04176c
NaamaHolzer <naamaholzer1@gmail.com>
2020-11-05 13:47:44 +0000

    Add 'should create' test to ToastService

--- capstone-project/src/main/angular-webapp/src/app/toast/toast.service.spec.ts
+++ capstone-project/src/main/angular-webapp/src/app/toast/toast.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Toast } from '@syncfusion/ej2-angular-notifications';
 
 import { ToastService } from './toast.service';
 
@@ -6,8 +7,10 @@ describe('ToastService', () => {
   let service: ToastService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToastService);
+    TestBed.configureTestingModule({
+      providers: [ToastService]
+    });
+    service = new ToastService();
   });
 
   it('should be created', () => {
