b7fd6ca60e37db6bb7da0d81fc084c63eae5ed24
NaamaHolzer <naamaholzer1@gmail.com>
2020-11-05 14:15:41 +0000

    Move MockInterceptor to an external file

--- capstone-project/src/main/angular-webapp/src/app/info-window/info-window.component.spec.ts
+++ capstone-project/src/main/angular-webapp/src/app/info-window/info-window.component.spec.ts
@@ -1,41 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import 'jasmine';
 
 import { InfoWindowComponent } from './info-window.component';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BlobAction } from '../blob-action';
-import { of } from 'rxjs/internal/observable/of';
 import { MarkerMode } from '../marker-mode';
-
-// Mock the HttpClient's interceptor so that HTTP requests are handled locally and not in the real back end.
-@Injectable()
-class MockInterceptor implements HttpInterceptor {
-
-  private responseUrl = {
-    imageUrl: "imageUrl"
-  }
-  private responseKey = {
-    blobKey: "blobKey"
-  }
-
-  constructor() { }
-
-  // Handles get / post requests
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    if (req.method === "GET" && req.url === '/blob-service?blobAction=' + BlobAction.GET_URL) {
-      return of(new HttpResponse({ status: 200, body: this.responseUrl }));
-    }
-    else if (req.method === "POST" && req.url === this.responseUrl.imageUrl) {
-      return of(new HttpResponse({ status: 200, body: this.responseKey }));
-    }
-
-    next.handle(req);
-  }
-}
+import { MockInterceptor } from '../mock-interceptor'
 
 describe('InfoWindowComponent', () => {
   let component: InfoWindowComponent;
