a08ec2c21557cdf40817b635f3485c59d68f9762
noyflat <noyflat@gmail.com>
2020-11-23 15:37:16 +0000

    Add test for deleting nonexsitent marker
    Add test for creating new marker

--- capstone-project/src/main/javaTests/com/google/sps/servlets/MarkerServletTest.java
+++ capstone-project/src/main/javaTests/com/google/sps/servlets/MarkerServletTest.java
@@ -1,9 +1,6 @@
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.*;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.gson.Gson;
@@ -20,8 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Map;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -35,9 +34,13 @@ public final class MarkerServletTest {
     private static Entity markerEntity;
     private static HttpServletRequest request;
     private static HttpServletResponse response;
-    private static MarkerServlet mockServlet;
     private static StringWriter stringWriter;
     private static PrintWriter writer;
+    private static Gson gson;
+    private static String markerJson;
+    private static final int CREATE_CODE = Action.CREATE.ordinal();
+    private static final int UPDATE_CODE = Action.UPDATE.ordinal();
+    private static final int DELETE_CODE = Action.DELETE.ordinal();
 
     @Before
     public void setUp() throws IOException {
@@ -61,10 +64,11 @@ public final class MarkerServletTest {
         markerEntity.setProperty("blobKey", "blobKey");
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
-        mockServlet = mock(MarkerServlet.class);
         stringWriter = new StringWriter();
         writer = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(writer);
+        gson = new Gson();
+        markerJson = gson.toJson(marker);
     }
 
     @After
@@ -73,29 +77,11 @@ public final class MarkerServletTest {
     }
 
     @Test
-    // Test the UPDATE case when the token ID is undefined
-    public void doPostCaseUpdateTokenUndefined() throws IOException {
-        Gson gson = new Gson();
-        String markerJson = gson.toJson(marker);
-        when(request.getParameter("marker")).thenReturn(markerJson);
-        when(request.getParameter("action")).thenReturn("1");
-        when(request.getParameter("userToken")).thenReturn("undefined");
-        MockServletContext mockServletContext = new MockServletContext();
-
-        MarkerServlet servlet = new MarkerServlet();
-        MarkerServlet spiedServlet = Mockito.spy(servlet);
-        Mockito.doReturn(mockServletContext).when(spiedServlet).getServletContext();
-        spiedServlet.doPost(request, response);
-
-        assertTrue(mockServletContext.throwable.toString().contains(new EntityNotFoundException(markerEntity.getKey()).toString()));
-    }
-
-    @Test
     // Test the doGet method when datastore is empty
     public void doGetDatastoreEmpty() throws IOException {
         new MarkerServlet().doGet(request, response);
 
-        assertTrue(stringWriter.toString().equals("[]\n"));
+        assertEquals("[]\n", stringWriter.toString());
     }
 
     @Test
@@ -104,12 +90,73 @@ public final class MarkerServletTest {
         DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
         ds.put(markerEntity);
 
-        Gson gson = new Gson();
-        String json = gson.toJson(marker);
-
         new MarkerServlet().doGet(request, response);
 
-        assertTrue(stringWriter.toString().contains(json));
+        assertTrue(stringWriter.toString().contains(markerJson));
     }
 
+    @Test
+    // Test the UPDATE case when the marker is not in datastore
+    public void doPostCaseUpdateNoMarker() throws IOException {
+        when(request.getParameter("marker")).thenReturn(markerJson);
+        when(request.getParameter("action")).thenReturn(Integer.toString(UPDATE_CODE));
+        when(request.getParameter("userToken")).thenReturn("undefined");
+        MockServletContext mockServletContext = new MockServletContext();
+
+        MarkerServlet servlet = new MarkerServlet();
+        MarkerServlet spiedServlet = Mockito.spy(servlet);
+        Mockito.doReturn(mockServletContext).when(spiedServlet).getServletContext();
+        spiedServlet.doPost(request, response);
+
+
+        assertTrue(mockServletContext.throwable.toString().contains(new EntityNotFoundException(markerEntity.getKey()).toString()));
+    }
+
+    @Test
+    // Test the DELETE case when the marker is not in datastore
+    public void doPostCaseDeleteNoMarker() throws IOException {
+        when(request.getParameter("marker")).thenReturn(markerJson);
+        when(request.getParameter("action")).thenReturn(Integer.toString(DELETE_CODE));
+        when(request.getParameter("userToken")).thenReturn("undefined");
+        when(request.getParameter("id")).thenReturn(Long.toString(marker.getId()));
+        MockServletContext mockServletContext = new MockServletContext();
+
+        MarkerServlet servlet = new MarkerServlet();
+        MarkerServlet spiedServlet = Mockito.spy(servlet);
+        Mockito.doReturn(mockServletContext).when(spiedServlet).getServletContext();
+        spiedServlet.doPost(request, response);
+
+
+        assertTrue(mockServletContext.throwable.toString().contains(new EntityNotFoundException(markerEntity.getKey()).toString()));
+    }
+
+    @Test
+    // Test the CREATE case when the user is undefined
+    public void doPostCaseCreateNoUser() throws IOException, EntityNotFoundException {
+        when(request.getParameter("marker")).thenReturn(markerJson);
+        when(request.getParameter("action")).thenReturn(Integer.toString(CREATE_CODE));
+        when(request.getParameter("userToken")).thenReturn("undefined");
+        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+        MockServletContext mockServletContext = new MockServletContext();
+        // Mock ServletContext or else we get an error
+        MarkerServlet servlet = new MarkerServlet();
+        MarkerServlet spiedServlet = Mockito.spy(servlet);
+        Mockito.doReturn(mockServletContext).when(spiedServlet).getServletContext();
+
+        // Preform the doPost and get the marker Id back so we can search for it
+        spiedServlet.doPost(request, response);
+        Map<String,String> responseParameter = gson.fromJson(String.valueOf(stringWriter), Map.class);
+        Key markerEntityKey = KeyFactory.createKey("Marker", Long.parseLong(responseParameter.get("id")));
+
+        // Create the entity identical to the one put in the datastore
+        markerEntity = new Entity("Marker", Long.parseLong(responseParameter.get("id")));
+        markerEntity.setProperty("lat", 1.0);
+        markerEntity.setProperty("lng", 1.0);
+        markerEntity.setProperty("animal", "animal");
+        markerEntity.setProperty("reporter", "reporter");
+        markerEntity.setProperty("description", "description");
+        markerEntity.setProperty("blobKey", "blobKey");
+
+        assertEquals(ds.get(markerEntityKey), markerEntity);
+    }
 }
